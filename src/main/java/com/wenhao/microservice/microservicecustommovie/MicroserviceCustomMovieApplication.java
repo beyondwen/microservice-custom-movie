5d10f8f572fbe6b2cd739be302250cb2c7b66530
wenhao176 <921790476@qq.com>
2018-12-06 17:08:33 +0800

    通过feign使用 rest 请求

--- src/main/java/com/wenhao/microservice/microservicecustommovie/MicroserviceCustomMovieApplication.java
+++ src/main/java/com/wenhao/microservice/microservicecustommovie/MicroserviceCustomMovieApplication.java
@@ -3,18 +3,13 @@ package com.wenhao.microservice.microservicecustommovie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class MicroserviceCustomMovieApplication {
-
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceCustomMovieApplication.class, args);
--- src/main/java/com/wenhao/microservice/microservicecustommovie/controller/MovieController.java
+++ src/main/java/com/wenhao/microservice/microservicecustommovie/controller/MovieController.java
@@ -1,21 +1,21 @@
 package com.wenhao.microservice.microservicecustommovie.controller;
 
 
+import com.wenhao.microservice.microservicecustommovie.dao.UserFeignClient;
 import com.wenhao.microservice.microservicecustommovie.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserFeignClient userFeignClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:9090/" + id, User.class);
+        return this.userFeignClient.findById(id);
     }
 }
